38a8e82b0534bfc862ee7195f9afb85c6742cab8
Itzelbelgax <itzelbuaim@gmail.com>
2026-02-22 18:17:38 -0700

    Agregado registro de usuarios y conexión BD

---
+++ guardar_usuario.php
@@ -0,0 +1,21 @@
+<?php
+
+include("conexion.php");
+
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO pacientes (nombre, telefono, email, password)
+        VALUES ('$nombre', '$telefono', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Usuario registrado correctamente";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+
+?>
