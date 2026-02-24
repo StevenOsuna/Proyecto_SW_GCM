986fcbdae9ba0779bbd9184918e8807bbc2ad6f1
Steven Osuna <osuna.steven95@gmail.com>
2026-02-23 19:26:43 -0700

    creacion de footer.php para separar e inclur en todas las paginas sin repetir y se cambio el index a .php

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-dark text-white text-center p-3">
+      <div class="container">
+        <h5 class="fw-bold">© 2026 Consultorio Médico</h5>
+      </div>
+</footer>
--- index.php
+++ index.php
@@ -64,11 +64,7 @@
 
 </main>
 
-<footer class="bg-dark text-white text-center p-3">
-    <div class="container">
-    <h5 class="fw-bold">© 2026 Consultorio Médico</h5>
-    </div>
-</footer>
+<?php include 'footer.php'; ?> 
 
 </body>
 
