f442c24098b074483d5afce2ff0b0821e4609156
DAVA <contacto@DAVA.com>
2026-02-27 16:14:39 -0600

    agrego mapa correcto

--- estructura/footer.php
+++ estructura/footer.php
@@ -41,7 +41,7 @@
                 <h5 class="fw-bold text-primary mb-4 text-start">Ubicación</h5>
                 <div class="shadow-sm overflow-hidden" style="border-radius: 15px;">
                     <iframe 
-                        src="https://maps.app.goo.gl/Fvkss2kL1wvKb43F6" 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.463691653138!2d-109.00474022425444!3d25.807888606473212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ba2603a1c03e1b%3A0x8804f1c716271a73!2sBenjam%C3%ADn%20Jhonston%201893%2C%20Bachomo%2C%2081247%20Los%20Mochis%2C%20Sin.!5e0!3m2!1ses-419!2smx!4v1715600000000!5m2!1ses-419!2smx" 
                         width="100%" height="180" style="border:0;" allowfullscreen="" loading="lazy">
                     </iframe>
                 </div>
