9d5c04521376a3f398f5f3993fbaf54ae04e9574
Steven Osuna <osuna.steven95@gmail.com>
2026-05-18 18:27:48 -0700

    agregue script de bootstrap a mis citas.php

--- paciente/mis_citas.php
+++ paciente/mis_citas.php
@@ -28,6 +28,7 @@ $resultado = $stmt->get_result();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="../assets/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body class="bg-light">
 
